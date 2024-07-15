b2865f3635af94f57aa7aa7254e71458b4458423
Priyanshu Sharma <priyanshush57@gmail.com>
2024-07-15 16:44:41 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud'
+        hostname: 'wry-ibis-720.convex.cloud'
       },
       {
         protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
+        hostname: 'wry-ibis-720.convex.cloud'
       },
       {
         protocol: 'https',
